77e73c2ebcf45f42b3e2bee028b22f185117bb2c
natchapon sridonchai <35339883+forceframe@users.noreply.github.com>
2018-01-24 22:32:55 +0700

    Add files via upload

---
+++ greatDivisor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int greatDivisor(int x){
+	int i = x-1;
+	while(i > x || i != 0){
+		if(x % i == 0){
+			return i;
+		}
+		i--;
+	}
+}
+
+int main(){
+	int x;
+	cout << "input = ";
+	cin >> x;
+	cout << "\nExpected output = " << greatDivisor(x);
+	return 0;
+}
+
+
